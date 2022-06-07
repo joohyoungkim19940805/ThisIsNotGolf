2adfff7e639ee311100f361112eb710e62409f3e
kim.joohyoung <kim.joohyoung@host.docker.internal>
2022-06-07 09:03:57 +0900

    [20220607] 배포테스트

--- project/src/main/java/com/hide_and_fps/project/router/MainRouter.java
+++ project/src/main/java/com/hide_and_fps/project/router/MainRouter.java
@@ -2,6 +2,7 @@ package com.hide_and_fps.project.router;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,9 +14,12 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 
 import com.hide_and_fps.project.handler.MainHandler;
 
+import reactor.core.publisher.Mono;
+
 @Configuration
 public class MainRouter {
 	
+	private AtomicInteger counter = new AtomicInteger(0);
 	
 	@Bean
 	public RouterFunction<ServerResponse> index1(MainHandler webFluxHandler){
@@ -36,5 +40,17 @@ public class MainRouter {
 		return RouterFunctions.route(RequestPredicates.GET("/homeTest")
 				.and(RequestPredicates.accept(MediaType.TEXT_HTML)),
 				webFluxHandler::homeTest);
+	}
+	
+	@Bean
+	public RouterFunction<ServerResponse> testApi(){
+		
+		return RouterFunctions.route(RequestPredicates.POST("/testApi"), 
+					req -> {
+						System.out.println(counter.get());
+						return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(
+							Mono.just( Map.ofEntries( Map.entry("resCount", counter.getAndIncrement()) ) )
+							, Map.class);
+							});
 	}
 }
