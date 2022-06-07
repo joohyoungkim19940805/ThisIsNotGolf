f861de8ca9fe4ef20d4498f497188dc7b5a63b5c
kim.joohyoung <kim.joohyoung@host.docker.internal>
2022-06-07 16:21:47 +0900

    [20220607] 웹플랙스 api 테스트 및 배포 테스트

--- project/src/main/java/com/hide_and_fps/project/router/MainRouter.java
+++ project/src/main/java/com/hide_and_fps/project/router/MainRouter.java
@@ -1,25 +1,33 @@
 package com.hide_and_fps.project.router;
 
+import java.awt.List;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.json.simple.JSONObject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import com.hide_and_fps.project.handler.MainHandler;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Configuration
 public class MainRouter {
 	
-	private AtomicInteger counter = new AtomicInteger(0);
+
 	
 	@Bean
 	public RouterFunction<ServerResponse> index1(MainHandler webFluxHandler){
@@ -43,14 +51,28 @@ public class MainRouter {
 	}
 	
 	@Bean
-	public RouterFunction<ServerResponse> testApi(){
-		
+	public RouterFunction<ServerResponse> testApi(MainHandler webFluxHandler){
+		return RouterFunctions.route(RequestPredicates.POST("/testApi")
+				.and(RequestPredicates.accept(MediaType.APPLICATION_JSON)),
+				webFluxHandler::testApi);
+		/*
 		return RouterFunctions.route(RequestPredicates.POST("/testApi"), 
 					req -> {
-						System.out.println(counter.get());
+						req.formData().subscribe(x->{
+							System.out.println(x);
+						});
 						return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(
 							Mono.just( Map.ofEntries( Map.entry("resCount", counter.getAndIncrement()) ) )
 							, Map.class);
 							});
+							*/
 	}
+	/*
+	@Setter
+	@Getter
+	@ToString
+	public static class User {
+	    private Map<String,Object> attributes;
+	}
+	*/
 }
